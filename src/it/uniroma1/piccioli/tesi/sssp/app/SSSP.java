69419550d626496acf1049e72215efcd1039e844
85piccio <85piccio@gmail.com>
2015-01-19 15:50:29 +0100

    Source da long a string (compatibilita con dataset gplus id=string)

--- src/it/uniroma1/piccioli/tesi/sssp/app/SSSP.java
+++ src/it/uniroma1/piccioli/tesi/sssp/app/SSSP.java
@@ -13,6 +13,8 @@ import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
+import it.uniroma1.piccioli.tesi.sssp.out.MapperOut;
+import it.uniroma1.piccioli.tesi.sssp.out.ReducerOut;
 import it.uniroma1.piccioli.tesi.sssp.spread.MapperSpread;
 import it.uniroma1.piccioli.tesi.sssp.spread.ReducerSpread;
 import it.uniroma1.piccioli.tesi.sssp.sync.MapperSync;
@@ -25,12 +27,13 @@ public class SSSP extends Configured implements Tool {
 	public int run(String[] args) throws Exception {
 
 		Configuration conf = this.getConf();
-//		long source = 0;
-		long source = 21651598;
+		String source = "0";
 		if (args.length > 2) {
-			source = Long.parseLong(args[2]);
+//			source = Long.parseLong(args[2]);
+			source = args[2];
 		}
-		conf.setLong(SOURCE_INDEX, source);
+		//conf.setLong(SOURCE_INDEX, source);
+		conf.setStrings(SOURCE_INDEX, source);
 		conf.setBoolean(VEDIT, false);
 
 		// path job1
@@ -42,25 +45,28 @@ public class SSSP extends Configured implements Tool {
 		// per entrare
 		boolean hasUpdates = true;
 		boolean justOne = true;
-//		int maxIter = 10;
+		// int maxIter = 10;
 		long prev = 1;
 		int nn = 0;
-		while (hasUpdates /*&& (maxIter > 1)*/) {
+		while (hasUpdates /* && (maxIter > 1) */) {
 
-			//reset cartelle di output
-			FileSystem fs = FileSystem.get(conf);			
-			fs.delete(outputFinale, true);
+			Path outtmp = new Path("/result_outtmp");
+
+			// reset cartelle di output
+			FileSystem fs = FileSystem.get(conf);
+			fs.delete(outtmp, true);
 			fs.delete(path1, true);
 
 			/*
-			 * Primo Job 
-			 * Input Lista di Archi nella forma "Sorgente Destinazione"
-			 * Mapper - (Sorgente,Destinazione) -> emit (Destinazione,Sorgene) 
-			 * Reducer - (v,[k,k,k,...] -> Calcola nodo sorgente con distanza minima e, per ogni k, emit(k,v) con k.val = distMin)
-			 * */
+			 * Primo Job Input Lista di Archi nella forma
+			 * "Sorgente Destinazione" Mapper - (Sorgente,Destinazione) -> emit
+			 * (Destinazione,Sorgene) Reducer - (v,[k,k,k,...] -> Calcola nodo
+			 * sorgente con distanza minima e, per ogni k, emit(k,v) con k.val =
+			 * distMin)
+			 */
 			Job jobInit = Job.getInstance(conf);
 
-			jobInit.setJobName("SSCP-step-"+ nn);
+			jobInit.setJobName("SSCP-step-" + nn);
 
 			jobInit.setMapperClass(MapperSpread.class);
 			jobInit.setReducerClass(ReducerSpread.class);
@@ -70,30 +76,30 @@ public class SSSP extends Configured implements Tool {
 			jobInit.setInputFormatClass(KeyValueTextInputFormat.class);
 			jobInit.setOutputFormatClass(TextOutputFormat.class);
 			FileInputFormat.addInputPath(jobInit, inputIniziale);
-			FileOutputFormat.setOutputPath(jobInit, outputFinale);
+			FileOutputFormat.setOutputPath(jobInit, outtmp);
 
 			jobInit.setMapOutputKeyClass(Text.class);
 			jobInit.setMapOutputValueClass(Text.class);
 
 			jobInit.waitForCompletion(true);
 
-			//Contatore degli update della variabile distMin nel reducer 
-			long updates = jobInit.getCounters().findCounter(STATE.UPDATED).getValue(); 
+			// Contatore degli update della variabile distMin nel reducer
+			long updates = jobInit.getCounters().findCounter(STATE.UPDATED).getValue();
 
 			/*
-			 * Secondo Job 
-			 * Input Lista di Archi nella forma "Sorgente Destinazione"
-			 * Mapper - (Sorgente,Destinazione) -> emit (Destinazione,Sorgene) & emit (Sorgente,Destinazione)
-			 * Reducer - (v,[k,k,k,...] -> Per ogni nodo calcola il valore minDist in modo da sincronizzare i valori dei nodi sorgenti in input)
-			 * */
+			 * Secondo Job Input Lista di Archi nella forma
+			 * "Sorgente Destinazione" Mapper - (Sorgente,Destinazione) -> emit
+			 * (Destinazione,Sorgene) & emit (Sorgente,Destinazione) Reducer -
+			 * (v,[k,k,k,...] -> Per ogni nodo calcola il valore minDist in modo
+			 * da sincronizzare i valori dei nodi sorgenti in input)
+			 */
 
 			Job jobSync = Job.getInstance(conf);
-			jobSync.setJobName("SSSP-sync-"+ nn);
-
+			jobSync.setJobName("SSSP-sync-" + nn);
 
 			jobSync.setJarByClass(SSSP.class);
 
-			FileInputFormat.addInputPath(jobSync, outputFinale);
+			FileInputFormat.addInputPath(jobSync, outtmp);
 			jobSync.setInputFormatClass(KeyValueTextInputFormat.class);
 
 			jobSync.setMapperClass(MapperSync.class);
@@ -107,35 +113,54 @@ public class SSSP extends Configured implements Tool {
 
 			jobSync.waitForCompletion(true);
 
-			//Inverto path input con path output per iterazione successiva
-			if(justOne){
-				inputIniziale =path2;//per preservare input originale 
-				justOne=false;
+			// Inverto path input con path output per iterazione successiva
+			if (justOne) {
+				inputIniziale = path2;// per preservare input originale
+				justOne = false;
 			}
 			Path tmpSwitch = path1;
 			path1 = inputIniziale;
 			inputIniziale = tmpSwitch;
-			
-            // Verifico che ci sono stati update altrimeni termino iterazioni
-            long diffPrev = updates - prev;
-            hasUpdates = (diffPrev != 0);
-            prev = updates;
-            
-            System.out.println("Round numero:" + nn);
-            nn++;           
+
+			// Verifico che ci sono stati update altrimeni termino iterazioni
+			long diffPrev = updates - prev;
+			hasUpdates = (diffPrev != 0);
+			prev = updates;
+
+			System.out.println("Round numero:" + nn);
+			nn++;
 
 		}
 
-		//"sposto" in cartella output e elimino cartelle temporanee
+		Job jobOut = Job.getInstance(conf);
+
+		jobOut.setJobName("SSSP-out");
+
+		jobOut.setJarByClass(SSSP.class);
+
+		FileInputFormat.addInputPath(jobOut, inputIniziale);
+		jobOut.setInputFormatClass(KeyValueTextInputFormat.class);
+
+		jobOut.setMapperClass(MapperOut.class);
+		jobOut.setReducerClass(ReducerOut.class);
+
+		jobOut.setOutputKeyClass(Text.class);
+		jobOut.setOutputValueClass(Text.class);
+
+		FileOutputFormat.setOutputPath(jobOut, outputFinale);
+		jobOut.setOutputFormatClass(TextOutputFormat.class);
+
+		jobOut.waitForCompletion(true);
+		
+
+		// pulisco cartelle temporanee
 		FileSystem fs = FileSystem.get(conf);
-		fs.rename(inputIniziale, outputFinale);		
 		fs.delete(path2, true);
 		fs.delete(path1, true);
-		
+
 		return 0;
 
 	}
-
 
 	public static void main(String[] args) throws Exception {
 		if (args.length < 2) {
@@ -156,7 +181,7 @@ public class SSSP extends Configured implements Tool {
 	}
 
 	static int printUsage() {
-		System.out.println("DegreeCalculator <input> <output>");
+		System.out.println("DegreeCalculator <input> <output> [<Source Index>]");
 		ToolRunner.printGenericCommandUsage(System.out);
 		return -1;
 	}
--- src/it/uniroma1/piccioli/tesi/sssp/spread/MapperSpread.java
+++ src/it/uniroma1/piccioli/tesi/sssp/spread/MapperSpread.java
@@ -13,7 +13,8 @@ public class MapperSpread extends Mapper<Text, Text, Text, Text> {
 	@Override
 	protected void map(Text key, Text value, Context context) throws IOException, InterruptedException {
 
-		long source = context.getConfiguration().getLong(SOURCE_INDEX, 0);
+		String[] sources = context.getConfiguration().getStrings(SOURCE_INDEX);
+		String source = sources[0];
 
 		if (key.toString().contains("-") && value.toString().contains("-")) {//valori gia inizializzati
 
@@ -24,10 +25,12 @@ public class MapperSpread extends Mapper<Text, Text, Text, Text> {
 			Text k = new Text(key.toString() + "-" + Long.MAX_VALUE);// FORMATO: "id-valore"
 			Text v = new Text(value.toString() + "-" + Long.MAX_VALUE);
 
-			if (Long.parseLong(key.toString()) == source) {
+//			if (Long.parseLong(key.toString()) == source) {
+			if (key.toString().equals(source)) {
 				k = new Text(key.toString() + "-0");
 			}
-			if (Long.parseLong(value.toString()) == source) {
+//			if (Long.parseLong(value.toString()) == source) {
+			if (value.toString().equals(source)) {
 				v = new Text(value.toString() + "-0");
 			}
 			context.write(v, k);
